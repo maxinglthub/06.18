2e33041da04b38fb1b5dc96c0c82dd80a824752c
MaxxHuang0801 <112845129+MaxxHuang0801@users.noreply.github.com>
2025-06-18 14:42:53 -0700

    first commit

---
+++ 0618.py
@@ -0,0 +1,21 @@
+sumUpFactorsOf = int(input("Pls enter a number: "))
+n = 0
+factor = 2
+limit = sumUpFactorsOf
+stop = True
+
+if factor == limit:
+    print(n)
+elif sumUpFactorsOf % factor == 0:
+    n = sumUpFactorsOf
+    factor = factor + 1
+
+
+while stop == True:
+    if sumUpFactorsOf % factor == 0:
+        n = n + sumUpFactorsOf
+        if factor == limit:
+            print(n)
+            stop = False
+    
+    factor = factor + 1
