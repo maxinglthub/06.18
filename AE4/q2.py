ee7e7c1479d5dff787d43e0307885360c59e60b2
MaxxHuang0801 <112845129+MaxxHuang0801@users.noreply.github.com>
2025-06-25 16:22:54 -0700

    question_2

--- AE4/q2.py
+++ AE4/q2.py
@@ -1,5 +1,16 @@
-def question2_function(s):
-    largestFibLessThan = 30
-    add = 0
+def largestFibLessThan(N):
+    save = 0
+    number = 0
+    ram = 0
+    add = 1
     
-    for i in 
+    for i in range(1, N - 1):
+        number = add + ram
+        if number >= N:
+            save = add
+            return save
+        ram = add
+        add = number
+
+ans = largestFibLessThan(30)
+print("The ans is: ", ans)
